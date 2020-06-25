a8ef58de5d289c94ebb4e16f9656023d959c5318
RaduTodor <33525159+RaduTodor@users.noreply.github.com>
2020-06-25 22:06:31 +0300

    1st Lab Homework

---
+++ Laborator 1 Windows Forms/Laborator 1 Windows Forms/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laborator_1_BDML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            dataSetList = new List<double[]>();
+            centroidList = new List<double[]>();
+            clustering = new List<int>();
+            colors = new Color[] { Color.Red, Color.Blue, Color.Green, Color.Gray, Color.Yellow, Color.Black, Color.Indigo, Color.Crimson, Color.Chocolate, Color.DarkSalmon };
+            InitializeComponent();
+        }
+
+        private List<double[]> dataSetList;
+        private List<double[]> centroidList;
+        private List<int> clustering;
+        private Color[] colors;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            Random objRandom = new Random();
+            dataSetList = new List<double[]>();
+            for (int i = 0; i < (int)numericUpDown1.Value; i++)
+            {
+                double[] point = new double[2];
+                for (int j = 0; j < 2; j++)
+                {
+                    point[j] = KMeansManager.GenerateRandomDouble(objRandom, 0, 400);
+                }
+                dataSetList.Add(point);
+            }
+
+            if (dataSetList.Count > 0 && centroidList.Count > 0)
+            {
+                clustering = KMeansManager.Cluster(dataSetList.ToArray(), centroidList.Count, centroidList.ToArray()).ToList();
+            }
+
+            picImage.Invalidate();
+        }
+
+        private void picImage_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            foreach (double[] point in dataSetList)
+            {
+                g.DrawEllipse(new Pen(Color.Gray, 2.0f), (float)point[0], (float)point[1], 10, 10);
+            }
+            foreach (double[] point in centroidList)
+            {
+                g.DrawEllipse(new Pen(Color.Red, 2.0f), (float)point[0], (float)point[1], 15, 15);
+            }
+            if (dataSetList.Count > 0 && centroidList.Count > 0)
+            {
+                for (int i = 0; i < clustering.Count; i++)
+                {
+                    g.DrawLine(new Pen(colors[clustering[i]], 1.0f), (float)dataSetList.ElementAt(i)[0], (float)dataSetList.ElementAt(i)[1], (float)centroidList.ElementAt(clustering[i])[0], (float)centroidList.ElementAt(clustering[i])[1]);
+                }
+            }
+        }
+
+
+        private void picImage_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            if (me.Button == MouseButtons.Left)
+            {
+                if (centroidList.Count < colors.Length)
+                    centroidList.Add(new double[] { Convert.ToDouble(me.X - 7), Convert.ToDouble(me.Y - 7) });
+            }
+            else if (me.Button == MouseButtons.Right)
+            {
+                var poz = centroidList.FindIndex(c => c[0] <= me.X + 8 && c[0] >= me.X - 8 && c[1] <= me.Y + 8 && c[1] >= me.Y - 8);
+                if (poz != -1)
+                    centroidList.RemoveAt(poz);
+            }
+
+            if (dataSetList.Count > 0 && centroidList.Count > 0)
+            {
+                clustering = KMeansManager.Cluster(dataSetList.ToArray(), centroidList.Count, centroidList.ToArray()).ToList();
+            }
+
+            picImage.Invalidate();
+        }
+
+
+        private void event_keyDown(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                for (int i = 0; i < centroidList.Count; i++)
+                {
+                    var list = dataSetList.Where(d => clustering[dataSetList.IndexOf(d)] == i);
+                    centroidList.ElementAt(i)[0] = list.Average(d => d[0]);
+                    centroidList.ElementAt(i)[1] = list.Average(d => d[1]);
+                }
+            }
+
+            picImage.Invalidate();
+        }
+    }
+}
---
+++ Laborator 1 Windows Forms/Laborator 1 Windows Forms/KMeansManager.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator_1_BDML
+{
+    public static class KMeansManager
+    {
+        public static double GenerateRandomDouble(Random random, double minimum, double maximum)
+        {
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+        public static int[] Cluster(double[][] rawData, int numClusters, double[][] means)
+        {
+
+            double[][] data = rawData;
+            bool changed = true;
+            int[] clustering = InitClustering(data.Length, numClusters, 0);
+            int maxCount = data.Length * 10;
+            int ct = 0;
+            while (changed == true && ct < maxCount)
+            {
+                ++ct;
+                changed = UpdateClustering(data, clustering, means);
+            }
+            return clustering;
+        }
+
+        private static int[] InitClustering(int numTuples, int numClusters, int randomSeed)
+        {
+            Random random = new Random(randomSeed);
+            int[] clustering = new int[numTuples];
+            for (int i = 0; i < numClusters; ++i)
+                clustering[i] = i;
+            for (int i = numClusters; i < clustering.Length; ++i)
+                clustering[i] = random.Next(0, numClusters);
+            return clustering;
+        }
+
+        private static bool UpdateClustering(double[][] data, int[] clustering, double[][] means)
+        {
+            int numClusters = means.Length;
+            bool changed = false;
+
+            int[] newClustering = new int[clustering.Length];
+            Array.Copy(clustering, newClustering, clustering.Length);
+
+            double[] distances = new double[numClusters];
+
+            for (int i = 0; i < data.Length; ++i)
+            {
+                for (int k = 0; k < numClusters; ++k)
+                    distances[k] = Distance(data[i], means[k]);
+
+                int newClusterID = MinIndex(distances);
+                if (newClusterID != newClustering[i])
+                {
+                    changed = true;
+                    newClustering[i] = newClusterID;
+                }
+            }
+
+            if (changed == false)
+                return false;
+
+            int[] clusterCounts = new int[numClusters];
+            for (int i = 0; i < data.Length; ++i)
+            {
+                int cluster = newClustering[i];
+                ++clusterCounts[cluster];
+            }
+
+            for (int k = 0; k < numClusters; ++k)
+                if (clusterCounts[k] == 0)
+                    return false;
+
+            Array.Copy(newClustering, clustering, newClustering.Length);
+            return true;
+        }
+
+        private static double Distance(double[] tuple, double[] mean)
+        {
+            double sumSquaredDiffs = 0.0;
+            for (int j = 0; j < tuple.Length; ++j)
+                sumSquaredDiffs += Math.Pow((tuple[j] - mean[j]), 2);
+            return Math.Sqrt(sumSquaredDiffs);
+        }
+
+        private static int MinIndex(double[] distances)
+        {
+            int indexOfMin = 0;
+            double smallDist = distances[0];
+            for (int k = 0; k < distances.Length; ++k)
+            {
+                if (distances[k] < smallDist)
+                {
+                    smallDist = distances[k];
+                    indexOfMin = k;
+                }
+            }
+            return indexOfMin;
+        }
+    }
+}
